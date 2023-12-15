d0c102d134ec41c8a98361f05ae01d6ea7840c46
Sebastien Blanc <scm.blanc@gmail.com>
2023-12-15 09:33:52 +0000

    adding kafka consumer

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using Confluent.Kafka;
 
-namespace dotnetcore
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        string bootstrapServers = "kafka-sebi-dev-devrel-sebastien.a.aivencloud.com:22235";
+        string groupId = "againother";
+        string topic = "othertopic";
+        string sslCaLocation = "ca.pem";
+        string sslCertLocation = "service.cert";
+        string sslKeyLocation = "service.key";
+
+        var config = new ConsumerConfig
         {
-            Console.WriteLine("Hello World!");
+            BootstrapServers = bootstrapServers,
+            GroupId = groupId,
+            AutoOffsetReset = AutoOffsetReset.Earliest,
+            SecurityProtocol = SecurityProtocol.Ssl,
+            SslCaLocation = sslCaLocation,
+            SslCertificateLocation = sslCertLocation,
+            SslKeyLocation = sslKeyLocation,
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe(topic);
+
+            Console.WriteLine($"Consuming messages from topic: {topic}");
+
+            while (true)
+            {
+                try
+                {
+                    var consumeResult = consumer.Consume();
+
+                    Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+                }
+                catch (ConsumeException e)
+                {
+                    Console.WriteLine($"Error while consuming message: {e.Error.Reason}");
+                }
+            }
         }
     }
 }
